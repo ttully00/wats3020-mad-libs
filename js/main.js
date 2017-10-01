87611b4193c9afebba27510e0307dc55b54a77ec
tullyt <tullyt@seattleu.edu>
2017-10-01 18:38:16 -0400

    mad lib changes

--- js/main.js
+++ js/main.js
@@ -19,7 +19,11 @@ let honorific = prompt('Please enter an honorific title (e.g. Mister or Colonel)
 let authorName = prompt('What is your full name?');
 
 // TODO: A variable named "adjective1" for a word used to describe something.
+<<<<<<< Updated upstream
 let adjective = prompt('Give me a word that describes something:');
+=======
+let adjective1 = prompt('Give me a word that describes something:');
+>>>>>>> Stashed changes
 // TODO: A variable named "vehicle" for a mode of transation (e.g. plane, boat, etc.).
 let vehicle = prompt('What is you favorite vehicle?');
 // TODO: A variable named "vehiclePart" for a part of the vehicle you just named.
@@ -68,13 +72,21 @@ let adjective5 = prompt('What is another word you would use to describe a vehicl
 // TODO: To begin with, prompt the user to collect their favorite number that is not zero.
 // Call this variable "userNumber".
 
+<<<<<<< Updated upstream
 let userNumber = prompt('What is your lucky number?');
+=======
+let userNumber = prompt('What is your lucky number?',3);
+>>>>>>> Stashed changes
 
 // TODO: generate TWO more numbers called "number2" and "number 3".
 // Use any operator you would like to add, multiply, divide, or otherwise
 // change the number that the user submitted.
 
+<<<<<<< Updated upstream
 let number2 = userNumber**4;
+=======
+let number2 = userNumber * 4;
+>>>>>>> Stashed changes
 
 let number3 = userNumber / number2 + (number2/12);
 
